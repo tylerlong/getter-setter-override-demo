0a2c6943031cccfa0335b575fd5a945486d37c2a
Tyler Long <tyler4long@gmail.com>
2021-03-26 16:16:26 -0700

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world');
+abstract class A {
+  _enabled = true;
+  get enabled() {
+    return this._enabled;
+  }
+  set enabled(value: boolean) {
+    this._enabled = value;
+  }
+}
+
+class B extends A {
+  set enabled(value: boolean) {
+    this._enabled = value;
+  }
+}
+
+const o = new B();
+console.log(o.enabled);
